a372c4fd8c511aaa076b3fafe4c2025fe34af627
Michael Wan <michaelw99@berkeley.edu>
2015-03-11 21:50:31 -0700

    done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,29 @@
 class UsersController < ApplicationController
-  # The controller for users.  Makes an implicit call to render "views/users/method_name.html.erb" at the end of every method.
 
   def index
-    @users = User.all # Get's all the Users in our database and assigns it the the instance variable @user.
+    @users = User.all 
   end
 
   def new
-    @user = User.new #Creates an empty user object.
+    @user = User.new 
   end
 
   def create
     @user = User.create(user_params)
-    if @user.save #If saving the user was successful
-      redirect_to @user #Go to the show view of the user
+    if @user.save 
+      redirect_to @user 
     else
-      render "new" #Go to the new view for the user
+      render "new" 
     end
   end
 
   def show
-    @user = User.find(params[:id]) # Query for the user with the id that is in params
+    @user = User.find(params[:id])
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email) #Returns a hash that was the value of "name" and "email" from the value of "user" in params.
+    params.require(:user).permit(:name, :email) 
   end
 end
